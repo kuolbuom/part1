0242878e3c2f74811100c697276d14f962f01e38
Student <student@sirafrica.org>
2026-02-20 14:54:21 +0300

    Initial commit

---
+++ courseinfo/src/App.jsx
@@ -0,0 +1,69 @@
+const Header = ({course}) => {
+  console.log(course)
+  return(
+    <div>
+      <h1><p>{course}</p></h1>
+    </div>
+  )
+}
+
+const Part = ({name,exercises}) => {
+  console.log(name,exercises)
+  return(
+    <div>{name} {exercises}</div>
+  )
+}
+
+const Content = (props) => {
+  console.log(props)
+  return(
+    <div>
+      <Part name={props.part1.name} exercises={props.part1.exercises}/>
+      <Part name={props.part2.name} exercises={props.part2.exercises}/>
+      <Part name={props.part3.name} exercises={props.part3.exercises}/>
+    </div>
+  )
+}
+
+const Total = (props) => {
+  console.log(props)
+  return(
+   <div>
+    <p>the total of exercises {props.part1.exercises + props.part2.exercises + props.part3.exercises}</p>
+   </div>
+  )
+}
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
+
+  return (
+ <div>
+    <Header course={course}/>
+    <Content
+      part1={part1}
+      part2={part2}
+      part3={part3}
+    />
+    <Total
+      part1={part1}
+      part2={part2}
+      part3={part3}
+    />
+ </div>
+  )
+}
+
+export default App
