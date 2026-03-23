b58f404c27bccb21ac3397818773d05cddb557f2
kuol buom <kuolbuom111@gmail.com>
2026-03-23 16:28:08 +0300

    content component refactored

---
+++ courseinfo/step2/src/App.jsx
@@ -0,0 +1,69 @@
+const Header = (props) => {
+  console.log('header props',props)
+  return(
+    <div><h1>{props.course}</h1></div>
+  )
+}
+
+const Part = ({ name, exercise  }) => {
+  console.log('part component prop', name, exercise)
+  return(
+    <div>
+       <p>
+        {name} {exercise}
+      </p>
+    </div>
+  )
+}
+
+const Content = (props) => {
+  console.log('content props', props)
+  return(
+    <div>
+       <Part name={props.part1} exercise={props.exercises1} />
+       <Part name={props.part2} exercise={props.exercises2} />
+       <Part name={props.part3} exercise={props.exercises3} />
+    </div>
+  )
+}
+
+const Total = (props) => {
+  console.log('Total props')
+  return(
+    <div>
+      <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
+    </div>
+  )
+}
+
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  return (
+    <div>
+      <Header course={course} />
+       <Content 
+         part1={part1}
+         exercises1={exercises1}
+         part2={part2}
+         exercises2={exercises2}
+         part3={part3}
+         exercises3={exercises3}
+       />
+      <Total
+        exercises1={exercises1}
+        exercises2={exercises2}
+        exercises3={exercises3}
+      />
+    </div>
+  )
+}
+
+export default App
