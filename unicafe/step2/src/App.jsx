36e657d685504e9259f1ad2f1d97cf5906e53b73
kuol buom <kuolbuom111@gmail.com>
2026-03-24 12:00:31 +0300

    average and positive created

---
+++ unicafe/step2/src/App.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react'
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const all = good + neutral + bad
+  const average = all===0? 0 : (good-bad)/all
+  const positive = all===0?0 : (good/all) * 100
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <button onClick={(e) => setGood(good + 1)}>good</button>
+      <button onClick={(e) => setNeutral(neutral + 1)}>neutral</button>
+      <button onClick={(e) => setBad(bad + 1)}>bad</button>
+      
+      <h1>statistics</h1>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+      <p>all {all}</p>
+      <p>average {average}</p>
+      <p>positive {positive}</p>
+    </div>
+  )
+}
+
+export default App
